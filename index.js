8829744b4d18ce711eb4bfae11f0aac993dc4645
ntthuan060102github <ntthuan060102.work@gmail.com>
2022-12-02 08:56:43 +0700

    fix bug Access blocked by CORS

--- index.js
+++ index.js
@@ -41,10 +41,12 @@ app.get("/", (req, res) => {
     res.send(`Selina - Profile Service (${process.env.app_env})`)
 })
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-  next()
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', "*")
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    next()
 })
 
 app.use(ROUTES_PREFIX + "", account_router)
