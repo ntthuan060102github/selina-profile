ba63dce8e46764e3e67fe88dc9b37a8373fcb554
ntthuan060102github <ntthuan060102.work@gmail.com>
2022-12-02 10:13:45 +0700

    Test: CORS

--- index.js
+++ index.js
@@ -27,7 +27,11 @@ mongoose.connect(
 app.use(express.json())
 app.use(helmet())
 app.use(morgan("common"))
-app.use(cors({ origin: true }));
+app.use(cors({
+    origin:'http://localhost:3000', 
+    credentials:true,
+    optionSuccessStatus:200
+}))
 
 // const upload = multer({
 //     storage: multer.memoryStorage()
@@ -40,7 +44,6 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
     res.setHeader('Access-Control-Allow-Credentials', true)
-    console.log(req.headers)
     next()
 })
 
